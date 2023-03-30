c940ae9ef64c97a305a043972a95f5f906b4eb8b
Leon_ <leonmagnificat@gmail.com>
2023-03-30 23:42:13 +0200

    search

--- src/components/groupPage/GroupPageCards.jsx
+++ src/components/groupPage/GroupPageCards.jsx
@@ -1,11 +1,12 @@
 import * as React from "react";
-import { Typography, Box, Button, Grid, Snackbar, Alert } from "@mui/material";
-import { styled } from "@mui/material/styles";
+import { Typography, Box, Button, Grid, Snackbar, Alert, InputBase, TextField } from "@mui/material";
+import { styled, alpha } from "@mui/material/styles";
 import { useState } from "react";
 import add from "../../icons/add.svg";
 import CreateGroupModel from "../Group/CreateGroupModel.jsx";
 import TheListOfMembersCard from "./TheListOfMembersCard.jsx";
 import { useSelector } from "react-redux";
+import SearchIcon from "@mui/icons-material/Search";
 
 const GroupBox = styled(Box)({
   height: "70px",
@@ -31,10 +32,13 @@ const AddButton = styled(Button)({
 export default function GroupPageCards(props) {
   const user = useSelector((state) => state.user.UserData);
   const groups = useSelector((state) => state.user.groups);
+  const members = useSelector((state) => state.user.members);
   const [open, setOpen] = useState(false);
   const [message, setMessage] = useState(false);
   const [infoText, setInfoText] = useState(false);
   const [color, setColor] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+  //const [searchResults, setSearchResults] = useState([]);
   const handleOpen = () => {
     setOpen(true);
   };
@@ -49,6 +53,58 @@ export default function GroupPageCards(props) {
 
     setMessage(false);
   };
+
+  const Search = styled("div")(({ theme }) => ({
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(1),
+      width: "auto",
+    },
+  }));
+
+  const SearchIconWrapper = styled("div")(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  }));
+
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: "inherit",
+    "& .MuiInputBase-input": {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create("width"),
+      width: "100%",
+      [theme.breakpoints.up("sm")]: {
+        width: "12ch",
+        "&:focus": {
+          width: "20ch",
+        },
+      },
+    },
+  }));
+
+  const searchResults = groups.filter((group) => {
+    group.members.map((member) => {
+      return member.firstName.toLowerCase().includes(searchTerm.toLowerCase()) || member.lastName.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+    return group.name.toLowerCase().includes(searchTerm.toLowerCase()) || group.currency.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+
+  console.log("searchResults", searchResults);
+
   return (
     <>
       <Box sx={{ flexGrow: 1 }}>
@@ -56,6 +112,19 @@ export default function GroupPageCards(props) {
           <Box sx={{ display: "flex", width: "100%", alignItems: "center", justifyContent: "space-between" }}>
             <Box>
               <Typography>All Groups ({groups && groups.length})</Typography>
+            </Box>
+            <Box>
+              <TextField
+                variant="standard"
+                placeholder="Search…"
+                onChange={(e) => {
+                  setSearchTerm(e.target.value);
+                }}
+              />
+              {/* {searchResults.length > 0 &&
+                searchResults.map((results) => {
+                  return <div>{results.name}</div>;
+                })} */}
             </Box>
             <Box>
               <AddButton variant="contained" onClick={handleOpen}>
@@ -66,7 +135,7 @@ export default function GroupPageCards(props) {
         </GroupBox>
 
         <Grid container spacing={2} columns={12}>
-          {groups.map((group) => {
+          {searchResults.map((group) => {
             return <TheListOfMembersCard key={group._id} group={group} user={user} setGroupsFunction={props.setGroupsFunction} setMessage={setMessage} setInfoText={setInfoText} />;
           })}
         </Grid>
